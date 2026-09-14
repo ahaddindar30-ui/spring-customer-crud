2614eace1ee37f136b0bdc06b5a3f518012227ee
ahaddindar30-ui <ahaddin30@gmail.com>
2026-09-14 16:06:46 +0330

    Clean repository and apply gitignore

--- src/main/java/com/ahad/banking/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ahad.banking.repository;
-
-import com.ahad.banking.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer,Integer> {
-
-}
